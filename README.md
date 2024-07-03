7ec577ff2a0b5fdffee142df4f0029c4051c422a
Julio Martínez <mizzag@gmail.com>
2024-07-03 15:13:50 -0600

    commit base

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# web_craping
+# Web Scraping
+---
+
+This is a repository to implement some web scraping technics from diferent web sites.
---
+++ WebScraping_1.py
@@ -0,0 +1,36 @@
+import requests
+from lxml import html
+
+# creating header
+encabezados = {
+    
+    "user-agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/71.0.3578.80 Chrome/71.0.3578.80 Safari/537.36",
+
+}
+
+# url + response
+url = 'https://www.wikipedia.org/'
+respuesta = requests.get(url, headers=encabezados)
+
+# creating parser
+parser = html.fromstring(respuesta.text)
+
+# get element by ID
+ingles = parser.get_element_by_id("js-link-box-en")
+print(ingles.text_content())
+
+# get element with xpath()
+ingles2 = parser.xpath("//a[@id='js-link-box-en']/strong/text()")
+print(ingles2)
+
+# get element by class with xpath
+idiomas = parser.xpath("//div[contains(@class, 'central-featured-lang')]//strong/text()")
+
+for idioma in idiomas:
+    print(idioma)
+
+# get element by class with find_class
+idiomas = parser.find_class('central-featured-lang')
+for i in idiomas:
+    print(i.text_content())
+    
