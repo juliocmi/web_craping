31ee9d4fde2034a639c53948f1ceba77fc051d9d
Julio Martínez <mizzag@gmail.com>
2024-07-03 16:17:45 -0600

    wikipedia v1.1

--- wscraping_v2.py
+++ wscraping_v2.py
@@ -3,7 +3,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-# create header
+# creating header
 header = {
     "user-agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/71.0.3578.80 Chrome/71.0.3578.80 Safari/537.36",
 }
